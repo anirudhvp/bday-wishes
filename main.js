b3a709134a79cbcdbd0d27c3f0eef7506f528f17
anirudhvp <78332865+anirudhvp@users.noreply.github.com>
2021-11-30 17:03:06 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var canvas = new fabric.Canvas('myCanvas');
  var x= document.getElementById("myAudio");
-
+var block_image_object="";
 function new_image()
 {
-	fabric.image.fromURL('BirthdayImage.jpg',function(img){
+	fabric.Image.fromURL('BirthdayImage.jpg',function(Img){
     block_image_object=Img;
     block_image_object.scaleToWidth(700);
-    block_image_object.scaleToHeigth(510);
+    block_image_object.scaleToHeight(510);
     block_image_object.set({
     top:0,
     left:0
